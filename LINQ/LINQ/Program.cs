b005d4927d1d3d66186b90cb86972c6b34fc1db8
bekkerVU <bekker.volodymyr.yu@gmail.com>
2025-03-26 11:01:33 +0200

    test encoding fix

---
+++ LINQ/LINQ/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Якийсь коментар державною мовою ґґҐ Її іІ єЄ
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+        }
+    }
+}
