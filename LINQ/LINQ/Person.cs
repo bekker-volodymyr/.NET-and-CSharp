e317649505b0c243800442175b871e681642c94e
bekkerVU <bekker.volodymyr.yu@gmail.com>
2025-03-26 12:55:27 +0200

    class files
    moved classes to separate files

---
+++ LINQ/LINQ/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace LINQ
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
--- LINQ/LINQ/Program.cs
+++ LINQ/LINQ/Program.cs
@@ -1,18 +1,5 @@
 ﻿namespace LINQ
 {
-    public class Person
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-    }
-
-    public class Product
-    {
-        public string Name { get; set; }
-        public decimal Price { get; set; }
-    }
-
     internal class Program
     {
         static void Main(string[] args)
